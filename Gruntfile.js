2251b6acae9d2dedfe2051c5252c8792896fda94
ivmartel <ivmartel@gmail.com>
2018-03-05 22:43:28 +0100

    Deactivate update script. Add js to lint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'src/*.js'],
+            files: ['Gruntfile.js', 'service-worker.js', 'src/*.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
