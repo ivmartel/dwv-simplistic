47a48e5cb4dde45558bf003e79ef092413e091ed
ivmartel <ivmartel@gmail.com>
2024-09-06 09:16:13 +0200

    Add comment

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,8 @@ const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// TODO: fix dwv web workers
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
